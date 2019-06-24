70e8c8d732ea8c0fecd826b51b143e7c2be85659
owlmeats <owlmeats@gmail.com>
2019-06-24 14:28:50 -0400

    file to print strings id diferent ways

---
+++ monday_afternoon_challenge.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+#Monday Afternoon Challenge:
+#Use the print function to return the following:
+#"Hello Texas!"
+#"Hi Sam, my name is _______.  WE are in Texas.  This is day 1"
+
+#Use the following variables: 
+#a = "Hello"
+#b = "Texas"
+#c = "Sam"
+#d = "{Your name}"
+#e = 1
+
+#Use 2 different ways to print it out
+
+
+a = "Hello"
+b = "Texas"
+c =  "Sam"
+d = "JP"
+e = 1
+
+
+#print statement 1
+z = "{} {}!".format(a,b)
+print(z)
+
+y = f"{a} {b}!"
+print(y)
+
+
+x = "Hi Sam, my name is {}.  We are in Texas.  This is day {}".format(d,e)
+print(x)
+
+w = f"Hi Sam, my name is {d}.  We are in Texas.  This is day {e}"
+print(w)
+
+
